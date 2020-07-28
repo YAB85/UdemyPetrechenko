5ffa493eb13223674baf5674c3cadd1010fae9cc
Yaroslav <boyaal24@gmail.com>
2020-07-28 16:04:23 +0300

    info about obj + methods

--- 2_BasicsForJS/original.js
+++ 2_BasicsForJS/original.js
@@ -23,3 +23,76 @@
 
 'use strict';
 
+/* function first() {
+    //Do something
+
+    setTimeout(function () {
+        console.log(1);
+    }, 500)
+}
+
+function second() {
+    console.log(2)
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`I learn : ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('I finished this lesson');
+}
+
+learnJS('JavaScript', done); */
+
+const obj = new Object();
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log('Test');
+    }
+};
+
+//options.makeTest();
+
+const {
+    border,
+    bg
+} = options.colors;
+console.log(border);
+console.log(bg);
+
+/* console.log(options.name);
+
+delete options.name;
+
+console.log(options); */
+
+
+/* let counter = 0;
+for (let key in options) {
+
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Properties ${i} have a value ${options[key][i]}`);
+            //counter++;
+        }
+    } else {
+        console.log(`Properties ${key} have a value ${options[key]}`);
+        counter++;
+    }
+} */
+
+//console.log(counter)
+
+//console.log(Object.keys(options).length);
